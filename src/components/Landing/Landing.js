4172226cc40d2ac181c37bf19e34b9b16975bb11
Matthew Crawford <crawfordm2000@gmail.com>
2019-12-13 20:51:03 -0500

    landing page styles

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -17,7 +17,7 @@ class Landing extends Component {
                     <span className="heading-primary--main">Cinema</span>
                     <span className="heading-primary--sub">ReelTalk is a place for movie lovers to form groups based on their common interests and discuss all topics related to film and televison.</span>
                 </h1>
-                <div classNameName='btn-wrap'>
+                <div className='btn-wrap'>
                 <a href="/signup" className="button button--white button--animated">Try</a>
                 <a href="/explore" className="button btn2 button--white button--animated">Explore</a>
                 </div>
