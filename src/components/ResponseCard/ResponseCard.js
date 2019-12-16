df8cccd652b33b480a27ad4ab5ec50ff2b3182c3
Matthew Crawford <crawfordm2000@gmail.com>
2019-12-16 10:58:05 -0500

    created response card, thread card, thread gallery

---
+++ src/components/ResponseCard/ResponseCard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ResponseCard extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default ResponseCard;
---
+++ src/components/ThreadGallery/ThreadGallery.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './ThreadGallery.css'
+
+class ThreadGallery extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default ThreadGallery;
