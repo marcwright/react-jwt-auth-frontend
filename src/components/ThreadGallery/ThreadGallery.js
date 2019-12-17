cd32bba01a8848acbafcf35afc1640e37b6cdc04
Matthew Crawford <crawfordm2000@gmail.com>
2019-12-17 07:42:44 -0500

    thread gallery initial setup

--- src/components/ThreadGallery/ThreadGallery.js
+++ src/components/ThreadGallery/ThreadGallery.js
@@ -11,22 +11,61 @@ class ThreadGallery extends Component {
     // }); 
         return ( 
             
-          <div>
+          <div className="thread-wrap">
 
-            <h3>Discussions</h3>
+        {/* INTRODCTION TO SIGN UP */}
+        <section className="section container " id="contact">
+          <div className="row">
+            <div className="col s12 l4 thread-form-wrap">
+            <form>
+              <h2>Create a discussion</h2>
+                <div className="input-field ">
+                  <i className="material-icons prefix">account_circle</i>
+                  <input type="text" id="text" />
+                  <label className="label" for="name">
+                    Title
+                  </label>
+                </div>
+                <div className="input-field">
+                  <i className="material-icons prefix">contacts</i>
+                  <input type="text" id="text" />
+                  <label className="label" for="name">
+                    Prompt
+                  </label>
+                </div>
+                
 
-          {/* FORM FOR CREATING NEW THREAD */}
-            <div id='thread-form-wrap' className='form-wrap'>
-
-
+                {/* SUBMIT BUTTON */}
+                <div className="btn-wrap">
+                  <button
+                    className="btn waves-effect waves-light"
+                    type="submit"
+                    name="action"
+                  >
+                    Create
+                  </button>
+                  
+                </div>
+              </form>
+              
             </div>
 
-            {/* GALLERY OF THREADS */}
-            {/* {ThreadCard} */}
 
 
-                
+            {/* SIGN UP FORM */}
+            <div className="col s12 l12 thread-gallery ">
+              <p>
+                The list of thread cards will go here
+              </p>
+              <p>
+                Mauris dolor augue, vulputate in pharetra ac, facilisis nec
+                libero. Fusce condimentum gravida urna, vitae scelerisque erat
+                ornare nec.
+              </p>
+            </div>
           </div>
+        </section>
+      </div>
                 
 
          );
