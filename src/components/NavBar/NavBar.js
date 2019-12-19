6c3cfcfcd1f5e437bd5d754419ec12b9cbf4ffcf
Matthew Crawford <crawfordm2000@gmail.com>
2019-12-19 17:35:27 -0500

    navbar links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,8 +7,8 @@ class NavBar extends Component {
   render() {
     let navBarItems = []
     if (this.props.isLoggedIn) {
-      navBarItems.push(<NavItem key={2} href='/logout'>LOG OUT</NavItem>, <NavItem key={4} href="/profile"><i className="material-icons prefix">account_circle</i></NavItem>)
       navBarItems.push(<NavItem key={7} href='/explore'>CLUBS</NavItem>)
+      navBarItems.push(<NavItem key={2} href='/logout'>LOG OUT</NavItem>, <NavItem key={4} href="/profile"><i className="material-icons prefix">account_circle</i></NavItem>)
       if (this.props.user != null) {
         navBarItems.push(<NavItem key={5} href='/profile'>{this.props.user.email}</NavItem>)
       }
@@ -17,7 +17,6 @@ class NavBar extends Component {
       navBarItems.push(<NavItem key={3} href='/signup'>SIGN UP</NavItem>)
       navBarItems.push(<NavItem key={6} href='/login'>LOG IN</NavItem>)
       navBarItems.push(<NavItem key={7} href='/explore'>CLUBS</NavItem>)
-      navBarItems.push(<NavItem key={8} href="/explore">MOVIES</NavItem>)
     }
     return (
       <Navbar brand='FILM CLUB' className='nav logo' right>
